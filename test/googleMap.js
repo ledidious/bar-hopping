b31600d97a56df9b3a68ca32384dd3cea60a9a93
Dieter <dieter.dederer@hs-osnabrueck.de>
2018-03-29 00:30:44 +0200

    add comments

--- test/googleMap.js
+++ test/googleMap.js
@@ -1,8 +1,8 @@
 var markers = [];
 
 /**
- * Create new marker and add theme to an map.
- * Also saves the marker in a array.
+ * Create new marker with fixed position and add it to the map
+ * Also saves the marker in a array
  * @param {google map} _map map to add marker
  */
 function createMarker(_map) {
@@ -17,17 +17,25 @@ function createMarker(_map) {
 }
 
 /**
- * removes all markers on map.
- * it will not delete the marker.
+ * Removes all markers on the map
+ * It will not delete the marker
  */
 function clearMarker() {
+    // loop throath all elements and removes them from the map
+    // by setting the associated map to null
     markers.forEach(element => {
         element.setMap(null);
     });
 
 }
 
+/**
+ * Adds a marker from array and moves to the marker
+ * @param {google map} _map map to add marker
+ * @param {string} selected city
+ */
 function addMarker(_map, selected) {
+    // JSON format
     let places = {
         "okinawa": {"lat": 26.352699, "lng": 127.811726},
         "germany": {"lat": 51.924477, "lng": 10.470170},
@@ -36,11 +44,14 @@ function addMarker(_map, selected) {
     };
 
     let marker = new google.maps.Marker({
-        position: places[selected],
+        position: places[selected], // requires lat and lng as JSON, see function createMarker
         animation: google.maps.Animation.DROP,
     });
 
+    // add the marker to the map
     marker.setMap(_map);
+    // save the marker
     markers.push(marker);
+    // move map to the marker
     _map.panTo(places[selected]);
 }
