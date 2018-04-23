861cc1634d81ada0363088f9f753ada0c9a1971d
Dieter <dieter.dederer@hs-osnabrueck.de>
2018-04-24 00:05:51 +0200

    Track user location and display blue dot as current pos

--- src/view/html/main.php
+++ src/view/html/main.php
@@ -16,26 +16,27 @@
 
             <!-- Profile -->
             <div id="profile" class="panel">
-                <?php //require_once "main/main_profile.php" ?>
+                <?php require_once "main/main_profile.php" ?>
             </div>
-<!---->
-<!--            <div id="tours" class="panel">-->
-<!--                --><?php //require_once "main/main_tours.php" ?>
-<!--            </div>-->
+
+            <div id="tours" class="panel">
+                <?php require_once "main/main_tours.php" ?>
+            </div>
         </div>
 
         <!-- Map. Referenced by javascript so keep identifier. -->
         <div id="map"></div>
 
-        <!-- Javascripts -->
-        <script src="../js/map/map.js"></script>
-
         <!-- Footer -->
         <?php require_once "footer.php" ?>
 
         <!-- Google maps js -->
-        <script async defer
-                src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAUfXbzl88V9EOUa2J6VCRYVRhKkQxzuCM&callback=initMap"></script>
+        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAUfXbzl88V9EOUa2J6VCRYVRhKkQxzuCM"></script>
+        <script src="../js/map/geolocation-marker.js"></script>
+        <script src="../js/map/map.js"></script>
+        <script>
+            initMap();
+        </script>
     </body>
 
 </html>
--- src/view/js/map/map.js
+++ src/view/js/map/map.js
@@ -12,41 +12,41 @@ function guidGenerator() {
     return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
 }
 
-var userPos;
-
 /**
  * map init function
  */
+
+let map, GeoMarker;
 function initMap() {
 
     // initial options
-    let map = new google.maps.Map(document.getElementById('map'), {
+    map = new google.maps.Map(document.getElementById('map'), {
         center: {
             lat: 47.4211,
             lng: 10.9852
         },
-        zoom: 20,
-        mapTypeControlOptions: {
-            position: google.maps.ControlPosition.TOP_RIGHT
-        },
+        zoom: 15,
+        mapTypeControlOptions: {position: google.maps.ControlPosition.TOP_RIGHT},
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
     });
 
-    // set current pos as center
-    if(navigator.geolocation) {
-        // watchId is used to clear the watch clearWatch(watchId)
-        let watchId = navigator.geolocation.watchPosition(function (_pos) {
-            let pos = {
-                lat: _pos.coords.latitude,
-                lng: _pos.coords.longitude
-            };
-
-            document.getElementById('profile').innerHTML = '[' + pos.lat + '] [' + pos.lng + ']';
-            console.log('[' + pos.lat + '] [' + pos.lng + ']');
-            map.panTo(pos);
-        }, showError, {enableHighAccuracy: true, maximumAge: 300, timeout: 27000});
+    // track user location and display blue dot ass current position
+    GeoMarker = new GeolocationMarker();
+    GeoMarker.setCircleOptions({fillColor: '#808080'});
+    google.maps.event.addListenerOnce(GeoMarker, 'position_changed', function () {
+        map.setCenter(this.getPosition());
+        map.fitBounds(this.getBounds());
+    });
+    google.maps.event.addListener(GeoMarker, 'geolocation_error', function (e) {
+        alert('There was an error obtaining your position. Message: ' + e.message);
+    });
+    GeoMarker.setMap(map);
+    google.maps.event.addDomListener(window, 'load', initMap);
+    if(!navigator.geolocation) {
+        alert('Your browser does not support geolocation');
     }
 
-    // add click event listener to create new loctions
+/*    // add click event listener to create new loctions
     google.maps.event.addListener(map, "click", function (_value) {
         // determine the location the user clicked
         let location = _value.latLng;
@@ -55,31 +55,13 @@ function initMap() {
             position: location,
             map: map,
         });
-        /**TODO: attach create method to click event
+        /!**TODO: attach create method to click event
          * process to create tour, rating, and pictures
-         */
+         *!/
 
         // attach click event handler to the marker
         google.maps.event.addListener(_marker, "click", function (e) {
             //TODO: add function to scrolle to the right location inside the list
         });
-    });
-}
-
-function showError(_error) {
-    switch(_error.code) {
-        case _error.PERMISSION_DENIED:
-            console.error("User denied the request for Geolocation.");
-            break;
-        case _error.POSITION_UNAVAILABLE:
-            console.error("Location information is unavailable.");
-            break;
-        case _error.TIMEOUT:
-            console.error("The request to get user location timed out.");
-            break;
-        case _error.UNKNOWN_ERROR:
-            console.error("An unknown error occurred.");
-            break;
-    }
-
+    });*/
 }
