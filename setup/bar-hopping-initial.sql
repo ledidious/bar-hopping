7942020c48f202e1d9939a6f9f89a4ab89655699
Jannis Brune <jannisbrune@t-online.de>
2018-05-02 15:46:15 +0200

    new version and dummy data

--- setup/bar-hopping-initial.sql
+++ setup/bar-hopping-initial.sql
@@ -25,67 +25,51 @@ CREATE TABLE IF NOT EXISTS `MARKER` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
--- Exportiere Daten aus Tabelle bar-hopping.MARKER: ~0 rows (ungefähr)
-DELETE FROM `MARKER`;
-/*!40000 ALTER TABLE `MARKER` DISABLE KEYS */;
-/*!40000 ALTER TABLE `MARKER` ENABLE KEYS */;
-
+-- Daten Export vom Benutzer nicht ausgewählt
 -- Exportiere Struktur von Tabelle bar-hopping.RATING
 CREATE TABLE IF NOT EXISTS `RATING` (
   `id` int(20) NOT NULL AUTO_INCREMENT,
   `imagePath` varchar(50) COLLATE utf8_bin DEFAULT NULL,
   `comment` text COLLATE utf8_bin,
   `value` int(1) NOT NULL,
-  `fk_TOURID` int(20) NOT NULL,
-  `fk_MARKERID` int(20) NOT NULL,
+  `fk_tourID` int(20) DEFAULT NULL,
+  `fk_markerID` int(20) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `fk_MARKERID02` (`fk_MARKERID`),
-  KEY `fk_TOURID02` (`fk_TOURID`),
-  CONSTRAINT `fk_MARKERID02` FOREIGN KEY (`fk_MARKERID`) REFERENCES `MARKER` (`id`),
-  CONSTRAINT `fk_TOURID02` FOREIGN KEY (`fk_TOURID`) REFERENCES `TOUR` (`id`)
+  KEY `fk_markerID02` (`fk_markerID`),
+  KEY `fk_tourID02` (`fk_tourID`),
+  CONSTRAINT `fk_markerID02` FOREIGN KEY (`fk_markerID`) REFERENCES `MARKER` (`id`),
+  CONSTRAINT `fk_tourID02` FOREIGN KEY (`fk_tourID`) REFERENCES `TOUR` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
--- Exportiere Daten aus Tabelle bar-hopping.RATING: ~0 rows (ungefähr)
-DELETE FROM `RATING`;
-/*!40000 ALTER TABLE `RATING` DISABLE KEYS */;
-/*!40000 ALTER TABLE `RATING` ENABLE KEYS */;
-
+-- Daten Export vom Benutzer nicht ausgewählt
 -- Exportiere Struktur von Tabelle bar-hopping.TOUR
 CREATE TABLE IF NOT EXISTS `TOUR` (
   `id` int(20) NOT NULL AUTO_INCREMENT,
-  `fk_USERID` int(20) NOT NULL,
+  `fk_userID` int(20) NOT NULL,
   `name` varchar(50) COLLATE utf8_bin DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `fk_USERID` (`fk_USERID`),
-  CONSTRAINT `fk_USERID` FOREIGN KEY (`fk_USERID`) REFERENCES `USER` (`id`)
+  KEY `fk_userID` (`fk_userID`),
+  CONSTRAINT `fk_userID` FOREIGN KEY (`fk_userID`) REFERENCES `USER` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
--- Exportiere Daten aus Tabelle bar-hopping.TOUR: ~0 rows (ungefähr)
-DELETE FROM `TOUR`;
-/*!40000 ALTER TABLE `TOUR` DISABLE KEYS */;
-/*!40000 ALTER TABLE `TOUR` ENABLE KEYS */;
-
+-- Daten Export vom Benutzer nicht ausgewählt
 -- Exportiere Struktur von Tabelle bar-hopping.TOUR2MARKER
 CREATE TABLE IF NOT EXISTS `TOUR2MARKER` (
-  `fk_TOURID` int(20) NOT NULL,
-  `fk_MARKERID` int(20) NOT NULL,
-  KEY `fk_MARKERID01` (`fk_MARKERID`),
-  KEY `fk_TOURID01` (`fk_TOURID`),
-  CONSTRAINT `fk_MARKERID01` FOREIGN KEY (`fk_MARKERID`) REFERENCES `MARKER` (`id`),
-  CONSTRAINT `fk_TOURID01` FOREIGN KEY (`fk_TOURID`) REFERENCES `TOUR` (`id`)
+  `fk_tourID` int(20) NOT NULL,
+  `fk_markerID` int(20) NOT NULL,
+  KEY `fk_markerID01` (`fk_markerID`),
+  KEY `fk_tourID01` (`fk_tourID`),
+  CONSTRAINT `fk_markerID01` FOREIGN KEY (`fk_markerID`) REFERENCES `MARKER` (`id`),
+  CONSTRAINT `fk_tourID01` FOREIGN KEY (`fk_tourID`) REFERENCES `TOUR` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
--- Exportiere Daten aus Tabelle bar-hopping.TOUR2MARKER: ~0 rows (ungefähr)
-DELETE FROM `TOUR2MARKER`;
-/*!40000 ALTER TABLE `TOUR2MARKER` DISABLE KEYS */;
-/*!40000 ALTER TABLE `TOUR2MARKER` ENABLE KEYS */;
-
+-- Daten Export vom Benutzer nicht ausgewählt
 -- Exportiere Struktur von Tabelle bar-hopping.USER
 CREATE TABLE IF NOT EXISTS `USER` (
   `id` int(20) NOT NULL AUTO_INCREMENT,
   `yearOfBirth` date DEFAULT NULL,
   `joinedSince` date NOT NULL,
-  `USERname` varchar(50) COLLATE utf8_bin NOT NULL,
+  `username` varchar(50) COLLATE utf8_bin NOT NULL,
   `password` varchar(50) COLLATE utf8_bin NOT NULL,
   `sex` char(10) COLLATE utf8_bin DEFAULT NULL,
   `email` varchar(50) COLLATE utf8_bin NOT NULL,
@@ -93,11 +77,7 @@ CREATE TABLE IF NOT EXISTS `USER` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
--- Exportiere Daten aus Tabelle bar-hopping.USER: ~0 rows (ungefähr)
-DELETE FROM `USER`;
-/*!40000 ALTER TABLE `USER` DISABLE KEYS */;
-/*!40000 ALTER TABLE `USER` ENABLE KEYS */;
-
+-- Daten Export vom Benutzer nicht ausgewählt
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
