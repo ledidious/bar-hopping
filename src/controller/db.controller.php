9f744a15a0e9f22ed38c4c60f54e3d718c1ba935
ledidious <ledidious@gmail.com>
2018-05-15 13:39:02 +0200

    Implement dbController and enable simple sql queries with sandbox folder

--- src/controller/db.controller.php
+++ src/controller/db.controller.php
@@ -2,17 +2,37 @@
 
 require_once __DIR__ . "/../../conf/db.conf.php";
 
-$servername = DB_SERVER_NAME;
-$dbName = DB_NAME;
-$username = DB_USERNAME;
-$password = DB_PASSWORD;
+class DbController {
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbName);
+    private static $_INSTANCE = null;
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    private $_sServerName = DB_SERVER_NAME;
+    private $_sDbName = DB_NAME;
+    private $_sUsername = DB_USERNAME;
+    private $_sPassword = DB_PASSWORD;
+
+    private $_oConnection;
+
+    public function __construct() {
+        $this->_oConnection = new mysqli($this->_sServerName, $this->_sUsername, $this->_sPassword, $this->_sDbName);
+
+        if ($this->_oConnection->connect_error) {
+            die("Connection failed");
+        }
+    }
+
+    public static function instance() {
+
+        if (self::$_INSTANCE == null) {
+            self::$_INSTANCE = new DbController();
+        }
+
+        return self::$_INSTANCE;
+    }
+
+    public function query($sSql) {
+        return $this->_oConnection->query($sSql);
+    }
 }
-echo "Connected successfully";
-?> 
+
+var_dump(DbController::instance()->query("select * from TOUR"));
