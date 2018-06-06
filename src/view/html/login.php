9169a0537d6d14f434874e7c6820c1dd57284e67
reinhardrauscher <ma.gierke@gmail.com>
2018-06-06 14:46:22 +0200

    login backdrisselt

--- src/view/html/login.php
+++ src/view/html/login.php
@@ -45,8 +45,7 @@
     </p>
         </form>
     <br>
-    Du hast dein Passwort vergessen? Dann klicke <a href="reset_password.php"> hier</a>.<br>
-    Du willst dich neu <a href="register.php">registrieren</a>?
+    Du hast dein Passwort vergessen? Dann klicke <a href="reset_password.php"> hier</a>.
 </fieldset>
 
 <!-- Footer -->
